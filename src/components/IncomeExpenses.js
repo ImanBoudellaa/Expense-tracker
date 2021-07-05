b9b0bd7c12275db614a8c1065a40a6d4e2cd7b36
ImanBoudellaa <boudellaiman@gmail.com>
2021-07-05 11:48:35 +0200

    expense, income, and balance done

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,24 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 export const IncomeExpenses = () => {
+
+  const {transactions} = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction=>transaction.amount)
+
+  const minus = Math.abs(amounts.filter(transaction=>transaction<0).reduce((acc, transaction)=> (acc+=transaction),0)).toFixed(2)
+  const plus = amounts.filter(transaction=>transaction>0).reduce((acc, transaction)=> (acc+=transaction),0).toFixed(2)
+
+
   return (
     <div className="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p className="money plus">+$0.00</p>
+      <p className="money plus">+${plus}</p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p className="money minus">-$0.00</p>
+      <p className="money minus">-${minus}</p>
     </div>
   </div>
   )
