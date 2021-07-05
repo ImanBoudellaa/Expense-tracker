024fe7390ab7300bdd300e1e65bf9622c7609995
ImanBoudellaa <boudellaiman@gmail.com>
2021-07-05 12:36:14 +0200

    added action for addtransaction and delete transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
 
+  const {addTransaction} = useContext(GlobalContext);
   const [text, setText]     = useState('');
   const [amount, setAmount] = useState(0)
 
 
+  const onSubmit=e=>{
+    e.preventDefault();
+    const newTransaction = {
+      //Ne trebam ovo radit al kad budem imala back bit ce lakse
+      id:Math.floor(Math.random()*100000000),
+      text,
+      amount: parseInt(amount)
+    }
+
+    addTransaction(newTransaction);
+  }
   return (
     <>
       <h3>Add new transaction</h3>
-      <form >
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
@@ -19,9 +32,9 @@ export const AddTransaction = () => {
           >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" value={amount}  onChange={(e)=>setAmount(e.target.value)} placeholder="Enter amount..." />
+          <input type="number" value={amount}  onChange={(e)=> setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className="btn" >Add transaction</button>
       </form>
 
     </>
